fb9cceb85a4f3b5e5a20fe664706c938caa291cd
fathlaziss <71271714+fathlaziss@users.noreply.github.com>
2021-02-23 20:55:25 +0700

    update detal_screen page

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -5,8 +5,62 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('Farm House Lembang'),
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            child: Text(
+              'Farm House Lembang',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Icon(Icons.calendar_today),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text('Open Everyday'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.access_time),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text('09.00 - 20.00'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.attach_money),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text('Rp 25.000'),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
+            ),
+          )
         ],
       ),
     );
